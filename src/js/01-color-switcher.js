3490e05c82f91c14e42a99906530452577077a5e
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-08-29 01:52:55 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let intervalId = 0;
 
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+stopBtn.disabled = true;
+
+function onStartBtn() {
+  switchActiveButton(stopBtn, startBtn);
+  changeBgColor(bodyRef);
+  intervalId = setInterval(() => {
+    changeBgColor(bodyRef);
+  }, 1000);
+}
+
+function onStopBtn() {
+  switchActiveButton(startBtn, stopBtn);
+  clearInterval(intervalId);
+}
+
+function switchActiveButton(firstBtn, secondBtn) {
+  if (firstBtn.disabled) {
+    firstBtn.disabled = false;
+    secondBtn.disabled = true;
+  }
+}
+
+function changeBgColor(element) {
+  element.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
